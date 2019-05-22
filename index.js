f69284556d131411fec3ffec24ac899c1553644f
Alex Fullalove <alexanderfullalove@gmail.com>
2019-05-22 11:11:49 +0100

    create handlePost function

--- index.js
+++ index.js
@@ -1,7 +1,17 @@
 const http = require("http");
 
 const server = http.createServer((req, res) => {
-  res.end("its working");
+  handlePost(req, ({ url }) => {});
 });
+
+const handlePost = (req, cb) => {
+  let body = "";
+  req.on("data", chunk => {
+    body += chunk.toString();
+  });
+  req.on("end", () => {
+    cb(JSON.parse(body));
+  });
+};
 
 module.exports = server;
